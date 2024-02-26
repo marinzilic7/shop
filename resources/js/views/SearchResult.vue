9a463261fad05628e0284a4334cf170b1053f992
marinzilic7 <marinzilic507@gmail.com>
2024-02-26 12:45:50 +0100

    update

--- resources/js/views/SearchResult.vue
+++ resources/js/views/SearchResult.vue
@@ -37,7 +37,7 @@
             </ul>
             <div class="card-body d-flex justify-content-between" style="background-color: #E4D9D9 !important;">
                 <a href="#"  class="card-link fs-4 text-muted text-decoration-underline">{{ product.price }}</a>
-                <a  href="#" @click="addCart(product.id)" class="card-link"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart cartSvg" viewBox="0 0 16 16">
+                <a  href="#" v-if="isUserLogin" @click="addCart(product.id)" class="card-link"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart cartSvg" viewBox="0 0 16 16">
   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
 </svg></a>
             </div>
@@ -52,7 +52,8 @@
         return {
             searchResults: [],
             isAdded:false,
-            message:''
+            message:'',
+            isUserLogin: false,
         };
     },
     mounted() {
@@ -75,6 +76,22 @@
                     this.isAdded = true;
                     console.log(this.message);
                 })
+        },
+        isUserLogged() {
+            axios
+                .get("/isUserLogged")
+                .then((response) => {
+                    this.message = response.data.message;
+                    this.isUserLogin= true;
+                    console.log(this.message)
+                    if(this.message === "Niste prijavljeni!"){
+                        this.isUserLogin = false;
+                    }
+
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
         },
 
     },
